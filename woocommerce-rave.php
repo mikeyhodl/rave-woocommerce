bdfa189612c39f1dc1cfa3dd5db33e0c9b72b214
Olaobaju Abraham <olaobajua@gmail.com>
2023-04-17 15:05:06 +0100

    update: plugin information comment and constants

--- woocommerce-rave.php
+++ woocommerce-rave.php
@@ -1,87 +1,85 @@
 <?php
+/**
+ * Plugin Name: Flutterwave WooCommerce
+ * Plugin URI: https://developer.flutterwave.com/
+ * Description: Official WooCommerce payment gateway for Flutterwave.
+ * Version: 2.3.2
+ * Author: Flutterwave Developers
+ * Author URI: http://flutterwave.com/us
+ * License: MIT License
+ * Text Domain: flw-payments
+ * WC requires at least:   3.0.0
+ * WC tested up to:        4.9.2
+ * Requires at least:      5.6
+ * Requires PHP:           7.4
+ *
+ * @package Flutterwave WooCommerce
+ **/
 
-/*
-Plugin Name: Flutterwave WooCommerce
-Plugin URI: https://rave.flutterwave.com/
-Description: Official WooCommerce payment gateway for Flutterwave.
-Version: 2.3.2
-Author: Flutterwave Developers
-Author URI: http://developer.flutterwave.com
-License: MIT License
-Text Domain: flw-payments
-WC requires at least:   3.0.0
-WC tested up to:        4.9.2
-*/
+defined( 'ABSPATH' ) || exit;
+define( 'FLUTTERWAVEACCESS', 1 );
+define( 'FLW_WC_PLUGIN_FILE', __FILE__ );
+define( 'FLW_WC_DIR_PATH', plugin_dir_path( FLW_WC_PLUGIN_FILE ) );
+define( 'FLW_WC_VERSION', '2.3.2' );
 
+/**
+ * Initialize Flutterwave WooCommerce payment gateway.
+ */
+function flw_woocommerce_rave_init() {
+	if ( ! class_exists( 'WC_Payment_Gateway' ) ) {
+		return;
+	}
 
-if (!defined('ABSPATH')) {
-  exit;
+	require_once FLW_WC_DIR_PATH . 'includes/class-flw-wc-payment-gateway.php';
+	if ( class_exists( 'WC_Subscriptions_Order' ) && class_exists( 'WC_Payment_Gateway_CC' ) ) {
+
+		require_once FLW_WC_DIR_PATH . 'includes/class-flw-wc-payment-gateway-subscriptions.php';
+
+	}
+
+	add_filter( 'woocommerce_payment_gateways', 'flw_woocommerce_add_rave_gateway', 99 );
 }
 
-define('FLW_WC_PLUGIN_FILE', __FILE__);
-define('FLW_WC_DIR_PATH', plugin_dir_path(FLW_WC_PLUGIN_FILE));
-define('FLW_WC_VERSION', '2.3.2');
-
-
-function flw_woocommerce_rave_init()
-{
-
-  if (!class_exists('WC_Payment_Gateway'))
-    return;
-
-  require_once(FLW_WC_DIR_PATH . 'includes/class.flw_wc_payment_gateway.php');
-
-  // include subscription if exists
-  if (class_exists('WC_Subscriptions_Order') && class_exists('WC_Payment_Gateway_CC')) {
-
-    require_once(FLW_WC_DIR_PATH . 'includes/class.flw_wc_subscription_payment.php');
-
-  }
-
-  add_filter('woocommerce_payment_gateways', 'flw_woocommerce_add_rave_gateway', 99);
-}
-add_action('plugins_loaded', 'flw_woocommerce_rave_init', 99);
+add_action( 'plugins_loaded', 'flw_woocommerce_rave_init', 99 );
 
 /**
  * Add the Settings link to the plugin
  *
- * @param  Array $links Existing links on the plugin page
+ * @param  array $links Existing links on the plugin page.
  *
- * @return Array          Existing links with our settings link added
+ * @return array Existing links with our settings link added
  */
-function flw_plugin_action_links($links)
-{
+function flw_plugin_action_links( array $links ): array {
 
-  $rave_settings_url = esc_url(get_admin_url(null, 'admin.php?page=wc-settings&tab=checkout&section=rave'));
-  array_unshift($links, "<a title='Flutterwave Settings Page' href='$rave_settings_url'>Settings</a>");
+	$rave_settings_url = esc_url( get_admin_url( null, 'admin.php?page=wc-settings&tab=checkout&section=rave' ) );
+	array_unshift( $links, "<a title='Flutterwave Settings Page' href='$rave_settings_url'>Settings</a>" );
 
-  return $links;
+	return $links;
 
 }
-add_filter('plugin_action_links_' . plugin_basename(__FILE__), 'flw_plugin_action_links');
+
+add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), 'flw_plugin_action_links' );
 
 /**
  * Add the Gateway to WooCommerce
  *
- * @param  Array $methods Existing gateways in WooCommerce
+ * @param  array $methods Existing gateways in WooCommerce.
  *
- * @return Array          Gateway list with our gateway added
+ * @return array Gateway list with our gateway added
  */
-function flw_woocommerce_add_rave_gateway($methods)
-{
+function flw_woocommerce_add_rave_gateway( array $methods ): array {
 
-  if (class_exists('WC_Subscriptions_Order') && class_exists('WC_Payment_Gateway_CC')) {
+	if ( class_exists( 'WC_Subscriptions_Order' ) && class_exists( 'WC_Payment_Gateway_CC' ) ) {
 
-    $methods[] = 'FLW_WC_Payment_Gateway_Subscriptions';
+		$methods[] = 'FLW_WC_Payment_Gateway_Subscriptions';
 
-  } else {
+	} else {
 
-    $methods[] = 'FLW_WC_Payment_Gateway';
-  }
+		$methods[] = 'FLW_WC_Payment_Gateway';
+	}
 
-  return $methods;
-
+	return $methods;
 }
 
 
-?>
+
