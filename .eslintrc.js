10cb40977073a91a6341890940f3823761ec6063
Olaobaju Abraham <olaobajua@gmail.com>
2023-04-24 22:04:57 +0100

    add external modules to eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,4 +9,16 @@ module.exports = {
 			{ definedTags: [ 'jest-environment' ] },
 		],
 	},
+	settings: {
+		// List of modules that are externals in our webpack config.
+		// This helps the `import/no-extraneous-dependencies` and
+		//`import/no-unresolved` rules account for them.
+		'import/core-modules': [
+			'@woocommerce/blocks-registry',
+			'@woocommerce/settings',
+			'@wordpress/i18n',
+			'@wordpress/is-shallow-equal',
+			'@wordpress/element',
+		],
+	},
 };
