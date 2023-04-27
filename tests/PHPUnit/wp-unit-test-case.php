d257cb61ed0a0654c3efff4452d5f3b6cca90671
Olaobaju Abraham <olaobajua@gmail.com>
2023-04-27 18:25:32 +0100

    add base testcase class via yoast

---
+++ tests/PHPUnit/wp-unit-test-case.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * PHPUnit bootstrap file
+ *
+ * @package Yoast\YoastSEO\Tests
+ */
+
+namespace PHPUnit;
+
+class WP_UnitTestCase extends \Yoast\PHPUnitPolyfills\TestCases\TestCase {
+
+}
