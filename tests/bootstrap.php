646b63698624757acbc2fa8180bc843efc0dc787
Olaobaju Abraham <olaobajua@gmail.com>
2023-04-27 19:31:57 +0100

    update phpunit bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,7 +5,7 @@
  * @package Flutterwave\WooCommerce\Tests
  */
 
- require_once __DIR__ . '/../../vendor/yoast/phpunit-polyfills/phpunitpolyfills-autoload.php';
+ require_once __DIR__ . '/../vendor/yoast/phpunit-polyfills/phpunitpolyfills-autoload.php';
 
 $_tests_dir = getenv( 'WP_TESTS_DIR' );
 
